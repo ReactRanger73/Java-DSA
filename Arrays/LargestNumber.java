3e50c8692463d304335e3beb66ed73f3daf240bc
Arzoo Kharayat <akharayat@conga.com>
2025-10-31 16:58:39 +0530

    Added Largest Number program

---
+++ Arrays/LargestNumber.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int largest = Integer.MIN_VALUE;
+        int[] arr = { 33, 7, 16, 12, 32, 55, 18, 26 };
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.printf("The largest number in the array is %d", largest);
+    }
+}
